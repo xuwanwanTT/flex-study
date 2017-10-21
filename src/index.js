5e52ce275479587ffc7ad958fb319d4dee3a60ca
xuwanwan <1054749044@qq.com>
2017-10-22 00:23:55 +0800

    完成justify-content和align-items

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 import './index.css'
-console.log(1)
+import $ from 'jquery'
+
+var $box = $('ul')
+
+$('.btn-init').on('click',function(){
+  $box.addClass('flex-init')
+  $(this).css('display','none')
+})
+
+$('.flex-jc').on('click','span',function(e){
+  var val = 'flex-jc-'+$(this).html()
+  var oval = 'flex-jc-'+$('.flex-jc > .active').html()
+  $box.removeClass(oval)
+  $(this).removeClass('active')
+  if(val !== oval){
+    $(this).addClass('active').siblings().removeClass('active')
+    $box.addClass(val)
+  }
+})
+
+$('.flex-ai').on('click','span',function(e){
+  var val = 'flex-ai-'+$(this).html()
+  var oval = 'flex-ai-'+$('.flex-ai > .active').html()
+  $box.removeClass(oval)
+  $(this).removeClass('active')
+  if(val !== oval){
+    $(this).addClass('active').siblings().removeClass('active')
+    $box.addClass(val)
+  }
+})
